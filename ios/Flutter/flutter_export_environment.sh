a249568d1f674860c8af8d28d62a27ca4528a2ba
Muhammad Salman <no@salman-g5.Home>
2020-07-03 17:33:19 +0500

    Pltform Specific Code TODO's Completed

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/no/Documents/flutter_linux/flutter_linux_1.17.2-stable/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/no/Documents/Flutter Projects/Flutter Assignments/Assignment 9 Flutter Platform Specific Code/flutter-week9-master/Flutter-method-channel-tutorial-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/no/Documents/flutter_linux/flutter_linux_1.17.2-stable/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
